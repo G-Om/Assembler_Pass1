6cbfabe5c26949acbbaf1320848f820ff950539d
Om <115257951+G-Om@users.noreply.github.com>
2022-10-09 12:22:55 +0530

    read from file

--- Test.java
+++ Test.java
@@ -1,9 +1,32 @@
 /**
  * Test
  */
-public class Test {
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 
+public class Test {
+    private BufferedReader br ;
+    public  void read() throws IOException{
+        System.out.println("File reader");
+        String line;
+        br = new BufferedReader(new FileReader("Input.txt"));
+        
+        while((line=br.readLine())!= null){
+            System.out.println(line);
+        }
+
+    }
     public static void main(String[] args) {
-        System.out.println("Hello Om How are you?");
+        Test t = new Test();
+        try {
+            t.read();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
     }
 }
